6ac0feed1fc55aa8fa6e4fba80cf5eafc2788da4
Artem <agudkovml@gmail.com>
2026-02-05 20:48:43 +0300

    homework №7

--- hw7.py
+++ hw7.py
@@ -6,6 +6,7 @@ import requests
 # Добавьте в класс Rate параметр diff, который в случае значения True в методах курсов валют будет возвращать изменение по сравнению в прошлым значением.
 # Считайте, self.diff будет принимать значение True только при возврате значения курса. При отображении всей информации о валюте он не используется.
 
+
 class Rate:
     def __init__(self, diff: bool = False):
         self.response = requests.get(
@@ -42,3 +43,36 @@ print(test.eur())
 # Напишите класс Designer, который учитывает количество международных премий для дизайнеров
 # Из презентации: "Повышение на 1 грейд за каждые 7 баллов. Получение международной премии – это +2 балла".
 # Считайте, что при выходе на работу сотрудник уже имеет две премии и их количество не меняется со стажем (конечно если хотите это можно вручную менять)
+
+
+class Designer:
+    def __init__(self, name, inter_award: int = 2, grade: int = 1):
+        self.name = name
+        self.inter_award = int(inter_award)
+        self.grade = int(grade)
+
+    def grade_up(self):
+        self.grade += 1
+
+    def publish_grade(self):
+        print(self.name, self.grade)
+
+    def needs_upgrade(self):
+        if self.inter_award * 2 > 7:
+            self.grade_up()
+
+        return self.publish_grade()
+
+    def inter_award_add(self, inter_award: int = 1):
+        self.inter_award += inter_award
+
+
+# Test
+
+a = Designer("Vanya")
+a.publish_grade()
+a.needs_upgrade()
+a.inter_award_add()
+a.needs_upgrade()
+a.inter_award_add(2)
+a.needs_upgrade()
